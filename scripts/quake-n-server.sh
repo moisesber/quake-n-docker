6eb0732151a20c62192e58a0e8075f339dde6daf
Moises Rodrigues <moisesr@spotify.com>
2020-12-16 12:54:00 +0100

    Change server run script defaults

--- scripts/quake-n-server.sh
+++ scripts/quake-n-server.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-GAME_DIR=${GAME_DIR:-modules}
+GAME_DIR=${GAME_DIR:-id1}
 MAP=${MAP:-e2m5}
 CONFIG_FILE=${CONFIG_FILE:-ctfcoord.cfg}
-ZONE=${ZONE:-512}
+ZONE=${ZONE:-1024}
 DEDICATED=${DEDICATED:-16}
 
 ./darkplaces-linux-686-dedicated -dedicated ${DEDICATED} -game ${GAME_DIR} -condebug -nojoy -zone ${ZONE} +exec ${CONFIG_FILE} map ${MAP}
